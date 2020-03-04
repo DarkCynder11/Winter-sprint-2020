d82b8539e1ce528babe0a5550f49866730427099
Mya Cordner-Cassidy <cordnerc@sheridancollege.ca>
2020-03-04 11:18:11 -0500

    added working file

---
+++ Sprint Wk - Winter 2020/Assets/Scripts/CharacterMovment.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovment : MonoBehaviour
+{
+    public float speed;
+    private Vector2 moveVelocity;
+
+    public GameObject Player;
+
+    public Vector3 scaleChangeLeft;
+    public Vector3 scaleChangeRight;
+
+    private Rigidbody2D cRB;
+
+    void Start()
+    {
+        cRB = GetComponent<Rigidbody2D>();
+        scaleChangeLeft = new Vector3(-1f, 1f, 1f);
+        scaleChangeRight = new Vector3(1f, 1f, 1f);
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(0, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * speed;
+
+        Vector2 changeDirection = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Player.transform.localScale = scaleChangeLeft;
+        }
+        else if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            Player.transform.localScale = scaleChangeRight;
+        }
+    
+    }
+
+    void FixedUpdate()
+    {
+        cRB.MovePosition(cRB.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+}
---
+++ Sprint Wk - Winter 2020/Assets/Scripts/Sword.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public float smooth = 1f;
+    private Quaternion targetRotation;
+
+    private bool rotate;
+
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        rotate = false;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            if (rotate)
+            {
+                targetRotation *= Quaternion.AngleAxis(90, Vector3.forward);
+                rotate = false;
+            }
+            else
+            {
+                targetRotation *= Quaternion.AngleAxis(-90, Vector3.forward);
+                rotate = true;
+            }
+        }
+        
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+    }
+
+}
+
